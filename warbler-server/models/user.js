81c2d2fe6cdb62237d2b8f7358ce5532fce58eb7
Mudassir Ahmed <ahmed.mudassir1999@gmail.com>
2020-10-13 17:46:14 +0530

    Added a pre save hook to hash the user password using bcrypt, also a helper method on the userSchema to compare the hashed password and if it returns true we login the user.

--- warbler-server/models/user.js
+++ warbler-server/models/user.js
@@ -21,6 +21,30 @@ const userSchema = new mongoose.Schema({
     }
 })
 
+// Hashing the password
+userSchema.pre('save', async function(next){
+    try{
+        if(!this.isModified('password')){
+            return next();
+        }
+        let hashPassword = await bcrypt.hash(this.password, 10);
+        this.password = hashPassword;
+        return next();
+    } catch(err){
+        return next(err);
+    }
+});
+// ================================================================
+
+userSchema.method.comparePassword = async function(candidatePassword,next){
+    try{
+        let isMatch = await bcrypt.compare(candidatePassword, this.password);
+        return isMatch;
+    }catch(err){
+        return next(err);
+    }
+}
+
 const User = mongoose.model('User',userSchema);
 
 module.exports = User;
