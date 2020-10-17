b262324493bb23488e13fae9e24c3575f3e4712d
Mudassir Ahmed <ahmed.mudassir1999@gmail.com>
2020-10-17 09:24:15 +0530

    Added loggint out functionality.

--- warbler-client/src/components/Homepage.js
+++ warbler-client/src/components/Homepage.js
@@ -1,16 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Homepage = () => {
+const Homepage = ({ currentUser }) => {
+  // user not logged in
+  if(!currentUser.isAuthenticated){
+    return (
+      <div className="home-hero">
+        <h1>What's Happening?</h1>
+        <h4>New to Warbler?</h4>
+        <Link to="/signup" className="btn btn-primary">
+          Sign up here
+        </Link>
+      </div>
+    );
+  }
   return (
-    <div className="home-hero">
-      <h1>What's Happening?</h1>
-      <h4>New to Warbler?</h4>
-      <Link to="/signup" className="btn btn-primary">
-        Sign up here
-      </Link>
+    <div>
+      <h1>LOGGED IN!!</h1>
     </div>
-  );
+  )
 };
 
 export default Homepage;
--- warbler-client/src/containers/Navbar.js
+++ warbler-client/src/containers/Navbar.js
@@ -2,8 +2,13 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Logo from '../images/warbler-logo.png';
+import { logout } from '../store/actions/auth';
 
 class Navbar extends Component{
+    logout = e => {
+        e.preventDefault();
+        this.props.logout();
+    }
     render(){
         return(
             <nav className='navbar navbar-expand' >
@@ -13,6 +18,18 @@ class Navbar extends Component{
                             <img src={Logo} alt="Warbler Home" />
                         </Link>
                     </div>
+                    {this.props.currentUser.isAuthenticated ? (
+                        <ul className="nav navbar-nav navbar-right">
+                            <li>
+                                <Link to={`/users/${this.props.currentUser.user.id}/messages/new}`}>New Message</Link>
+                            </li>
+                            <li>
+                                <a onClick={this.logout}>Log Out</a>
+                            </li>
+                        </ul>
+                    )
+                    :
+                    (
                     <ul className="nav navbar-nav navbar-right">
                         <li>
                             <Link to="/signup">Sign Up</Link>
@@ -21,9 +38,10 @@ class Navbar extends Component{
                             <Link to="/signin">Log In</Link>
                         </li>
                     </ul>
+                    )}
                 </div>
             </nav>
-        )
+        );
     }
 }
 
@@ -33,4 +51,4 @@ function mapStateToProps(state){
     };
 }
 
-export default connect(mapStateToProps, null)(Navbar);
+export default connect(mapStateToProps, { logout })(Navbar);
